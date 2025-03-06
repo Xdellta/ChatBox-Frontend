51aeffb1649dcf3004c15f811b89e91699b3a635
Patryk Piotrowski <xdelllta@gmail.com>
2025-03-06 21:28:55 +0100

    ChatBox logo

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import LogoIcon from '@/assets/icons/LogoIcon.vue';
+</script>
 
 <template>
   <h1>Login View</h1>
+  <LogoIcon class="logo" />
 </template>
 
-<style scoped></style>
+<style scoped>
+  .logo {
+    width: 100px;
+    height: max-content;
+  }
+</style>
